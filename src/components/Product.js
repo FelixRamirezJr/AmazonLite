c51d9236e21fa00b42c1280812fd844ff5813dc6
sk8withaguitar <Felix.Ramirezjr@gmail.com>
2017-05-30 00:19:11 -0700

    Create components for Product and Product Listing

---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+class ProductListing extends Component {
+  render() {
+    return (
+      <div> This will be a listing </div>
+    );
+  }
+}
+
+export default ProductListing;
---
+++ src/components/ProductListing.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+class Product extends Component {
+  render() {
+    return (
+      <div> This will be a listing </div>
+    );
+  }
+}
+
+export default Product;
