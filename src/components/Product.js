abc4d20c23f1ce96002a33ab010636f1d933d60e
= <felix.ramirezjr@gmail.com>
2017-06-01 03:13:08 -0700

    Right side column for product details now has the price, buy now and a go back button

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
 import Loading from './Loading';
 import SimpleGallery from './SimpleGallery';
 
@@ -10,18 +10,42 @@ class Product extends Component {
   constructor(props){
     super(props);
     this.state = {
-      productSet: false
+      productSet: false,
+      goBack: false
     };
+    this.goBack = this.goBack.bind(this);
+  }
+
+  goBack(){
+    this.setState({ goBack: true });
   }
 
   render() {
 
     /* Set gallery images */
+    if( this.state.goBack ){
+      return <Redirect to="/"/>;
+    }
     var images = util.getGalleryProductImages( this.props.item.ImageSets );
+
+    var price = "Not available";
+    if( this.props.item.ItemAttributes.ListPrice ){
+      price = this.props.item.ItemAttributes.ListPrice.FormattedPrice;
+    }
+
+    var dims = "Not available";
+    if (this.props.item.ItemAttributes.ItemDimensions != undefined){
+      dims = <span>
+                      {this.props.item.ItemAttributes.ItemDimensions.Length["_"]}x
+                      {this.props.item.ItemAttributes.ItemDimensions.Width["_"]}x
+                      {this.props.item.ItemAttributes.ItemDimensions.Height["_"]} in {" "}
+                      {this.props.item.ItemAttributes.ItemDimensions.Height["$"].Units}
+                    </span>;
+    }
 
     return (
       <div className="product">
-        <div className="col s12 m7 left-container">
+        <div className="col s12 m7 left-container white-object">
           <SimpleGallery exampleImage={this.props.item.LargeImage.URL} images={images} />
           <div className="info">
             <div className="title darker"> {this.props.item.ItemAttributes.Title } </div>
@@ -32,21 +56,23 @@ class Product extends Component {
             <div className="description lighter"> {this.props.item.ItemAttributes.Manufacturer} </div>
             <div className="title darker"> Dimensions (LxWxH) </div>
             <div className="description lighter">
-              {this.props.item.ItemAttributes.ItemDimensions.Length["_"]}x
-              {this.props.item.ItemAttributes.ItemDimensions.Width["_"]}x
-              {this.props.item.ItemAttributes.ItemDimensions.Height["_"]} in {" "}
-              {this.props.item.ItemAttributes.ItemDimensions.Height["$"].Units}
+              {dims}
             </div>
-            <div className="title darker"> Amazon URL </div>
+            <div className="title darker"> EAN </div>
             <div className="description lighter">
-              <a className="truncate" href={this.props.item.DetailPageURL}> 
-                {this.props.item.DetailPageURL}
-              </a>
+              {this.props.item.ItemAttributes.EAN}
             </div>
           </div>
         </div>
-        <div className="col s6 push-s2">
-
+        <div className="col s12 m4 offset-m1 right-container white-object">
+          <div className="price-title lighter"> List Price </div>
+          <div className="info">
+            <div className="price darker"> {price} </div>
+            <a target="_blank" className="btn full" href={this.props.item.DetailPageURL}>
+              Buy Now
+            </a>
+            <button className="btn full" onClick={this.goBack}> Go back </button>
+          </div>
         </div>
       </div>
     );
